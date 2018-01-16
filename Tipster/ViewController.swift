710abda8c6848ee1479b557d102dce33e40621e8
Matthew Burnside <matthewsburnside@gmail.com>
2018-01-16 15:45:33 -0800

    Added all variables and functions needed to calculate and format tips

--- Tipster/ViewController.swift
+++ Tipster/ViewController.swift
@@ -9,17 +9,75 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //    MARK: Properties
+    @IBOutlet var billLabel: UILabel!
+    var billAmount = "0"
+    
+    @IBOutlet var lowPercentageLabel: UILabel!
+    @IBOutlet var midPercentageLabel: UILabel!
+    @IBOutlet var highPercentageLabel: UILabel!
+    
+    @IBOutlet var lowTipLabel: UILabel!
+    @IBOutlet var midTipLabel: UILabel!
+    @IBOutlet var highTipLabel: UILabel!
+    
+    @IBOutlet var lowTotalLabel: UILabel!
+    @IBOutlet var midTotalLabel: UILabel!
+    @IBOutlet var highTotalLabel: UILabel!
+    
+    
+    @IBOutlet var groupSizeLabel: UILabel!
+    var groupMemberNumber = 1
+    var taxRate = 0.10
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        groupSizeLabel.text = "Group Size: \(groupMemberNumber)"
+        calculate()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //    MARK: Actions
+    @IBAction func tipSlider(_ sender: UISlider) {
+        taxRate = Double(sender.value)
+        calculate()
     }
-
-
+    
+    @IBAction func groupSizeSlider(_ sender: UISlider) {
+        groupMemberNumber = Int(round(sender.value))
+        groupSizeLabel.text = "Group Size: \(groupMemberNumber)"
+        calculate()
+    }
+    
+    @IBAction func billAmountInput(_ sender: UIButton) {
+        if sender.currentTitle == "c" {
+            billAmount = "0"
+            billLabel.text = billAmount
+            calculate()
+            return
+        }
+        if billLabel.text == "0" {
+            billAmount = sender.currentTitle!
+            billLabel.text = billAmount
+            calculate()
+        }
+        else {
+            billAmount.append(sender.currentTitle!)
+            billLabel.text = billAmount
+            calculate()
+        }
+    }
+    func calculate() {
+        lowPercentageLabel.text = String(Int(round(taxRate * 100))) + "%"
+        midPercentageLabel.text = String(Int(round(taxRate * 100 + 5))) + "%"
+        highPercentageLabel.text = String(Int(round(taxRate * 100 + 10))) + "%"
+        
+        lowTipLabel.text = String(format: "%.02f", (Double(billAmount)! * (Double(taxRate)))/Double(groupMemberNumber))
+        midTipLabel.text = String(format: "%.02f", (Double(billAmount)! * (Double(taxRate + 0.05)))/Double(groupMemberNumber))
+        highTipLabel.text = String(format: "%.02f", (Double(billAmount)! * (Double(taxRate + 0.10)))/Double(groupMemberNumber))
+        
+        lowTotalLabel.text = String(format: "%.02f", ((Double(billAmount)! * (Double(taxRate) + 1)))/Double(groupMemberNumber))
+        midTotalLabel.text = String(format: "%.02f", (Double(billAmount)! * (Double(taxRate + 0.05) + 1))/Double(groupMemberNumber))
+        highTotalLabel.text = String(format: "%.02f", (Double(billAmount)! * (Double(taxRate + 0.10) + 1))/Double(groupMemberNumber))
+    }
 }
-
